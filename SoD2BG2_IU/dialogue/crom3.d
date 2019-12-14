e297f78e319d05e5cdf883d884a89fce5db25176
DaerosTrollkiller <53156712+DaerosTrollkiller@users.noreply.github.com>
2019-12-14 16:48:28 -0500

    Update to v0.6.0
    Update of mod files to version 0.6.0

--- SoD2BG2_IU/dialogue/crom3.d
+++ SoD2BG2_IU/dialogue/crom3.d
@@ -1,11 +1,11 @@
 EXTEND_BOTTOM WSMITH01 13
-  IF ~PartyHasItem("bdboot01")~ THEN GOTO NewItem 	//Soft Feet +1
+  IF ~PartyHasItem("bdboot01")~ THEN GOTO NewItem 	//Quicksilver Sabatons
 END
   
 APPEND WSMITH01
   IF ~~ THEN BEGIN NewItem SAY @7
-    IF ~!PartyHasItem("chan12")~ THEN GOTO NewItem2
-    IF ~PartyHasItem("chan12")~ THEN GOTO NewItem3
+    IF ~!PartyHasItem("compon01")~ THEN GOTO NewItem2
+    IF ~PartyHasItem("compon01")~ THEN GOTO NewItem3
   END
 
   IF ~~ THEN BEGIN NewItem2 SAY @8
@@ -18,9 +18,9 @@ APPEND WSMITH01
                                                  SetGlobal("ForgeStuff","GLOBAL",1)
                                                  TakePartyGold(7500)
                                                  TakePartyItemNum("bdboot01",1)
-                                                 TakePartyItemNum("chan12",1)
+                                                 TakePartyItemNum("compon01",1)
                                                  DestroyItem("bdboot01")
-                                                 DestroyItem("chan12")
+                                                 DestroyItem("compon01")
                                                  DestroyGold(7500)~ GOTO 56
     IF ~~ THEN REPLY #67013 GOTO NoThanks
   END
