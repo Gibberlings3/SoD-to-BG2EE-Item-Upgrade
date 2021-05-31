a5b4c61311b3fcfb362772de03d5c7a556c0b760
DaerosTrollkiller <53156712+DaerosTrollkiller@users.noreply.github.com>
2021-05-31 13:36:30 -0400

    Update package_mod.bat
    updated packaging to include BBEE compatibility

--- package_mod.bat
+++ package_mod.bat
@@ -11,7 +11,7 @@ set mod_folder=sod2bg2_iu
 
 REM /* MODIFY: list here which IE games the mod is compatible with, from this list of possibilities: */
 REM /* %bg1%, %bg2%, %bgt%, %tutu%, %bgee%, %bgiiee%, %iwd1%, %iwdee%, %iwd2%, %iwd2ee%, %iwd-in-bg2%, %pst%, %pstee%, %eet% */
-set "compatible_games=%bgiiee% or %eet%"
+set "compatible_games=%bgiiee% or %eet%" or %bgee%
 
 REM /* MODIFY: these variables have sensible default values but they may need to be tweaked to match your particular mod */
 REM /* the ico_folder is where your .ico files are stored, which is usually the mod's backup or style folder */
